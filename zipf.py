d20da3946231e3d0845de23ce08ac8387e435061
jessicakeodara <keodarajessica@gmail.com>
2026-09-25 16:35:35 -0700

    Working on 1b

--- zipf.py
+++ zipf.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+from spacy.lang.en import English
+nlp = English(pipeline=[], max_length=5000000)
 
 import math
 import os.path
 from collections import Counter
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot
 
 
 def H_approx(n):
@@ -15,16 +20,69 @@ def H_approx(n):
     return gamma + math.log(n) + 0.5/n - 1./(12*n**2) + 1./(120*n**4)
 
 def do_zipf_plot(counts, label=""):
-    pass
+    fig = pyplot.figure()
+
+    # obtain C(w), R(w), and K(w)
+    count_freq = []
+    for elem, count in counts.most_common():  # wait i dont know for sure
+        count_freq.append(count)
+    print("count_freq: " + str(count_freq))
+    
+    rel_freq = []
+    for f in count_freq:
+        rel_freq.append(f / counts.total())
+
+    counts_size = len(counts.items())
+    rank = list(range(1, counts_size+1))
+
+
+    print("rank: " + str(rank))
+    print("rel_freq: " + str(rel_freq))
+        
+    # Create log plot of R(w) vs. K(w)
+    pyplot.loglog(rank, rel_freq)
+    pyplot.xlabel("log(rank)")
+    pyplot.ylabel("log(freq)")
+    pyplot.suptitle(label)
+
+    pyplot.savefig('zipf_{}.png'.format(label))
+    pyplot.close()
+    
+
 
 def read_all(directory, extension=None):
-    pass
+    new_counter = Counter()
+
+    for root, dirs, files in os.walk(directory):
+        print("dirs" + str(dirs))
+        for dir in dirs:
+            for file in files:
+                if extension is None:
+                    file_counter = read_one(file)
+                if os.path.splitext(dir + file)[1] == extension:
+                    file_counter = read_one(file)
+
+                new_counter += file_counter
+    return new_counter
+    
 
 def read_one(fname):
-    pass
+    # Read the file
+    with open(fname, 'r', encoding='latin1') as fp: 
+        text = fp.read()
+    
+    # Put tokens in counter
+    doc = nlp(text)
+    counter = Counter()
+    for token in doc:
+        counter[token.text.lower()] += 1
+    
+    return counter
+
 
 def plot_all(directory):
     counts = read_all(directory, ".txt")
+    print(counts)
     do_zipf_plot(counts, os.path.basename(directory))
 
 def plot_one(fname):
@@ -34,8 +92,8 @@ def plot_one(fname):
     do_zipf_plot(counts, label=title)
 
 def main():
-    plot_one('/cs/cs159/data/gutenberg/carroll-alice.txt')
-    plot_all('/cs/cs159/data/gutenberg')
+    plot_one('/courses/cs159/data/gutenberg/carroll-alice.txt')
+    plot_all('/courses/cs159/data/gutenberg')
 
 
 if __name__ == "__main__":
